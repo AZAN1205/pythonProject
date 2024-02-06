cebd6e5d10c01d6308bb51bf169d1cce8d9df3f0
AZAN1205 <156790580+AZAN1205@users.noreply.github.com>
2024-02-06 20:58:45 +0530

    Add files via upload

---
+++ Azan.py
@@ -0,0 +1,13 @@
+str = "Hello world!"
+print (str)
+print(str[0])
+print(str[2:5])
+print(str[2:])
+list = ["abcd",786,2.23,"john",70.2]
+tinylist = [123,"john"]
+print(list)
+print(list[0])
+print(list[1:3])
+print(list[2:])
+print(tinylist*2)
+print(list+tinylist)
---
+++ IF Condition.py
@@ -0,0 +1,14 @@
+User_name = input("Enter your name:")
+Password = input("Enter your password:")
+option = input("select any option A/B/C/D/E")
+if (option=="A"):
+    print("Add Book")
+elif ( option=="B"):
+    print("Update Book")
+elif (option=="C"):
+    print("DLT Book")
+elif (option=="D"):
+    print("View Book")
+
+
+
